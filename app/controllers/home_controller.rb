83b251a5e1571bf76054a9855c05030044c86419
machamanikanta <mmacha1@uiowa.edu>
2025-11-14 15:43:32 -0600

    route setup for heroku

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,10 @@
 class HomeController < ApplicationController
   def index
-    if (respond_to?(:user_signed_in?) && user_signed_in?) || respond_to?(:current_user) && current_user
-      redirect_to dashboard_path and return
+    if respond_to?(:current_user) && current_user
+      redirect_to dashboard_path
+    else
+      # explicit, just to be clear
+      render :index
     end
   end
 end
