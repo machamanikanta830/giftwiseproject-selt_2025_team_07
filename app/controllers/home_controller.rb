6dbeaf26a93a1a4c75d4f835e2fc6bb456a00862
machamanikanta <mmacha1@uiowa.edu>
2025-11-14 19:32:10 -0600

    tests/coverage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,9 @@
 class HomeController < ApplicationController
   def index
-    if (respond_to?(:user_signed_in?) && user_signed_in?) || respond_to?(:current_user) && current_user
-      redirect_to dashboard_path and return
+    if respond_to?(:current_user) && current_user
+      redirect_to dashboard_path
+    else
+      render :index
     end
   end
 end
--- features/step_definitions/landing_steps.rb
+++ features/step_definitions/landing_steps.rb
@@ -1,3 +1,13 @@
+# features/step_definitions/landing_steps.rb
+
+Given('I am on the home page') do
+  visit root_path
+end
+
+# Then('I should see {string}') do |text|
+#   expect(page).to have_content(text)
+# end
+
 Then('I should see a {string} button') do |button_text|
   begin
     expect(page).to have_link(button_text)
@@ -10,11 +20,21 @@ Then('the page should have a section with id {string}') do |section_id|
   expect(page).to have_css("##{section_id}", visible: :all)
 end
 
+# When('I click {string}') do |link_text|
+#   click_link(link_text)
+# end
+
 Then('I should be at the {string} section') do |section_id|
   expect(page).to have_css("##{section_id}", visible: true)
 end
 
 Then('I should see the step {string} with title {string}') do |step_number, title|
-  expect(page).to have_content(step_number)
-  expect(page).to have_content(title)
+  within('#how-it-works') do
+    expect(page).to have_content(step_number)
+    expect(page).to have_content(title)
+  end
+end
+
+Then('I should see the auth modal') do
+  pending "Wire up the auth modal and add a real selector here, e.g. '.auth-modal'"
 end
