af8eef2389314eda6b357654871d5c6df3496777
Yashwanth Kumar Mogili <yashwanthkumar-mogili@uiowa.edu>
2025-11-19 21:17:51 -0600

    Add password reset tokens table with constraints

--- db/migrate/20251119041438_create_password_reset_tokens.rb
+++ db/migrate/20251119041438_create_password_reset_tokens.rb
@@ -2,14 +2,13 @@ class CreatePasswordResetTokens < ActiveRecord::Migration[7.1]
   def change
     create_table :password_reset_tokens do |t|
       t.references :user, null: false, foreign_key: true
-      t.string :token
-      t.datetime :expires_at
-      t.boolean :used
+      t.string :token, null: false
+      t.datetime :expires_at, null: false
+      t.boolean :used, default: false, null: false
 
       t.timestamps
     end
-  end
-  def down
-    drop_table :password_reset_tokens
+
+    add_index :password_reset_tokens, :token, unique: true
   end
 end
